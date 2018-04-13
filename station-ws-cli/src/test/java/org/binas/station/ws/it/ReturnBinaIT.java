f1ead454e6902468e8e01a1f7e08043a7318167d
Manuel Sousa <manuelvsousa@tecnico.ulisboa.pt>
2018-04-13 11:53:48 +0100

    Add Tests for ReturnBina in station ws (closes #20)

---
+++ station-ws-cli/src/test/java/org/binas/station/ws/it/ReturnBinaIT.java
@@ -0,0 +1,49 @@
+package org.binas.station.ws.it;
+
+import org.binas.station.ws.BadInit_Exception;
+import org.binas.station.ws.NoSlotAvail_Exception;
+import org.binas.station.ws.StationView;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ReturnBinaIT extends BaseIT {
+	private static final int STATION_BONUS = 1000;
+	private static final int CAPACITY = 1;
+
+	@Before
+	public void setup(){
+		try {
+			client.testInit(10, 10, CAPACITY, STATION_BONUS);
+		} catch (BadInit_Exception e) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void validReturn() {
+		try {
+			client.getBina();
+			int bonus = client.returnBina();
+			Assert.assertEquals(bonus,STATION_BONUS);
+			Assert.assertEquals(client.getInfo().getTotalReturns(),1);
+			Assert.assertEquals(client.getInfo().getAvailableBinas(),CAPACITY);
+			Assert.assertEquals(client.getInfo().getTotalGets(),1);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+
+    @Test(expected = NoSlotAvail_Exception.class)
+    public void noSlotAvailable() throws NoSlotAvail_Exception {
+        try {
+            client.returnBina();
+        } catch (NoSlotAvail_Exception e) {
+            throw e;
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+
+}
