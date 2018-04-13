7a8b0e610d1512615d90303629a78a07f4f17b82
Manuel Sousa <manuelvsousa@tecnico.ulisboa.pt>
2018-04-13 01:02:54 +0100

    Fix Tests for GetInfoStation (#13)

--- binas-ws-cli/src/test/java/org/binas/ws/it/GetInfoStationIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/GetInfoStationIT.java
@@ -9,16 +9,17 @@ import org.junit.Test;
 public class GetInfoStationIT extends BaseIT {
 	private static final String STATION_ID = "T06_Station1";
 	private static final int CAPACITY = 10;
-	private static final Integer COORDINATE_X = 5;
-	private static final Integer COORDINATE_Y = 10;
+	private static final int COORDINATE_X = 5;
+	private static final int COORDINATE_Y = 10;
 	private static final String VALID_EMAIL = "testing1@text.com";
+	private static final String VALID_EMAIL_2 = "testing2@text.com";
 	private StationView st = null;
 
 	@Before
 	public void setUp() {
 		try {
 			client.testInit(10);
-			client.testInitStation(STATION_ID, COORDINATE_X, COORDINATE_Y, CAPACITY, 1);
+			client.testInitStation(STATION_ID, 5, 10, CAPACITY, 1);
 			this.st = client.getInfoStation(STATION_ID);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -30,8 +31,8 @@ public class GetInfoStationIT extends BaseIT {
 		try {
 			Assert.assertEquals(this.st.getCapacity(), CAPACITY);
 			Assert.assertEquals(this.st.getAvailableBinas(), CAPACITY);
-			Assert.assertEquals(this.st.getCoordinate().getX(), COORDINATE_X);
-			Assert.assertEquals(this.st.getCoordinate().getY(), COORDINATE_Y);
+			Assert.assertEquals(this.st.getCoordinate().getX(), new Integer(COORDINATE_X));
+			Assert.assertEquals(this.st.getCoordinate().getY(), new Integer(COORDINATE_Y));
 			Assert.assertEquals(this.st.getId(), STATION_ID);
 			Assert.assertEquals(st.getTotalReturns(),0);
 			Assert.assertEquals(st.getTotalGets(),0);
@@ -48,11 +49,15 @@ public class GetInfoStationIT extends BaseIT {
 			StationView st = client.getInfoStation(STATION_ID);
 			Assert.assertEquals(st.getAvailableBinas(), CAPACITY - 1);
 			Assert.assertEquals(st.getTotalGets(),1);
-			Assert.assertEquals(st.getFreeDocks(),CAPACITY - 1);
+			Assert.assertEquals(st.getFreeDocks(),1);
+			client.rentBina(STATION_ID,VALID_EMAIL_2);
+			StationView st2 = client.getInfoStation(STATION_ID);
+			Assert.assertEquals(st2.getAvailableBinas(), CAPACITY - 2);
+			Assert.assertEquals(st2.getTotalGets(),2);
+			Assert.assertEquals(st2.getFreeDocks(),2);
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		Assert.fail();
 	}
 
 	@Test
@@ -63,13 +68,11 @@ public class GetInfoStationIT extends BaseIT {
 			StationView st = client.getInfoStation(STATION_ID);
 			Assert.assertEquals(st.getAvailableBinas(), CAPACITY);
 			Assert.assertEquals(st.getTotalGets(),1);
-			Assert.assertEquals(st.getFreeDocks(),CAPACITY);
 			Assert.assertEquals(st.getTotalReturns(),1);
-
+			Assert.assertEquals(st.getFreeDocks(),0);
 		} catch (Exception e) {
 			Assert.fail();
 		}
-		Assert.fail();
 	}
 
 	@Test(expected = InvalidStation_Exception.class )
