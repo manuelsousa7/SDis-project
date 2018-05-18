d19d28d67902f55587e6b47f931a86ca5b78d555
goncasmage1 <goncalo.alexandre.marques@tecnico.ulisboa.pt>
2018-05-18 10:02:17 +0100

    Add evil handler

---
+++ ws-handlers/src/main/java/example/ws/handler/EvilHandler.java
@@ -0,0 +1,114 @@
+package example.ws.handler;
+
+import org.w3c.dom.Node;
+import pt.ulisboa.tecnico.sdis.kerby.*;
+import pt.ulisboa.tecnico.sdis.kerby.cli.KerbyClient;
+import pt.ulisboa.tecnico.sdis.kerby.cli.KerbyClientException;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.*;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.io.PrintStream;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+import java.util.Set;
+
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
+
+public class EvilHandler implements SOAPHandler<SOAPMessageContext> {
+
+    /** Date formatter used for outputting time stamp in ISO 8601 format. */
+    String kerby = "http://sec.sd.rnl.tecnico.ulisboa.pt:8888/kerby";
+    String fakeClient = "evilPotato@T06.binas.org";
+    String server = "binas@T06.binas.org";
+
+    SessionKeyAndTicketView requestedTicket = null;
+
+    public static final String CLIENT_HEADER = "clientHeader";
+    public static final String CLIENT_HEADER_NS = "http://clientHeader.com";
+    public static final String CLIENT_BODY = "clientBody";
+    public static final String CLIENT_BODY_NS = "http://clientBody.com";
+    public static final String TICKET_HEADER = "clientTicketHeader";
+    public static final String TICKET_NS = "http://ticket.com";
+    public static final String AUTH_HEADER = "clientAuthHeader";
+    public static final String AUTH_NS = "http://auth.com";
+
+    /**
+     * Gets the header blocks that can be processed by this Handler instance. If
+     * null, processes all.
+     */
+    @Override
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    private void logSOAPMessage(SOAPMessageContext smc, PrintStream out) {}
+
+    /** The handleFault method is invoked for fault message processing. */
+    @Override
+    public boolean handleFault(SOAPMessageContext smc) {
+        logSOAPMessage(smc, System.out);
+        return true;
+    }
+
+    /**
+     * Called at the conclusion of a message exchange pattern just prior to the
+     * JAX-WS runtime dispatching a message, fault or exception.
+     */
+    @Override
+    public void close(MessageContext messageContext) {
+        // nothing to clean up
+    }
+
+    /**
+     * Check the MESSAGE_OUTBOUND_PROPERTY in the context to see if this is an
+     * outgoing or incoming message. Write the SOAP message to the print stream. The
+     * writeTo() method can throw SOAPException or IOException.
+     */
+    @Override
+    public boolean handleMessage(SOAPMessageContext smc) {
+
+        Boolean outbound = (Boolean) smc.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+        if (outbound) {
+            try {
+
+                SOAPEnvelope se = smc.getMessage().getSOAPPart().getEnvelope();
+                SOAPHeader sh = se.getHeader();
+                SOAPBody sb = se.getBody();
+
+                if (sb == null) {
+                    sb = se.addBody();
+                }
+
+                Name clientName = se.createName(CLIENT_BODY, "e", CLIENT_BODY_NS);
+                System.out.println();
+                System.out.println(sb.getChildNodes().item(0).getTextContent());
+                System.out.println(sb.getChildNodes().item(1).getTextContent());
+                System.out.println();
+                Node node = sb.getChildNodes().item(1);
+                node.setTextContent(fakeClient);
+                System.out.println();
+                System.out.println(sb.getChildNodes().item(0).getTextContent());
+                System.out.println(sb.getChildNodes().item(1).getTextContent());
+                System.out.println();
+
+                System.out.println("[INFO] Kerby Url: " + kerby);
+                System.out.println("[INFO] Client email: " + fakeClient);
+                System.out.println("[INFO] Server Url: " + server);
+                System.out.println();
+
+            } catch (SOAPException e) {
+                System.out.printf("Failed to get SOAP header because of %s%n", e);
+            }
+        }
+        else {
+            //TODO: ?
+        }
+        return true;
+    }
+}
